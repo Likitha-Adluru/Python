4578e80463e9560194deb0cca70dea8f37845d00
Likitha-Adluru <likithaadluru1512@gmail.com>
2024-11-27 20:30:38 +0530

    Add files via upload

---
+++ Assignment 3 Output.py
@@ -0,0 +1,19 @@
+Python 3.12.7 (tags/v3.12.7:0b05ead, Oct  1 2024, 03:06:41) [MSC v.1941 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> 
+======= RESTART: C:/Users/likit/OneDrive/Documents/PYTHON/Assignment 3.py ======
+Task ID 1 status updated to completed.
+Task ID 2 due date updated to 2024-12-05.
+
+--- Task Report Grouped by Status ---
+
+Status: Pending
+Task ID: 3, Description: Write user documentation, Due Date: 2024-11-25
+
+Status: In progress
+Task ID: 2, Description: Develop login feature, Due Date: 2024-12-05
+
+Status: Completed
+Task ID: 1, Description: Design project layout, Due Date: 2024-12-01
+Task ID: 4, Description: Test payment system, Due Date: 2024-11-15
+No task found with the due date 2024-11-20.
